527f8918e20dcc290b3cd621d01c83d4e5c0590e
Aymane Achfennar <aachfenn@c1r4p5.1337.ma>
2024-07-09 12:15:51 +0100

    sleep bwtwen games

--- game-js/racket-mv.js
+++ game-js/racket-mv.js
@@ -76,6 +76,12 @@ document.addEventListener('keyup', function(event) {
             break;
     }
 });
+
+
+let newTopRightUp;
+let newTopRightDown;
+let newTopLeftUp;
+let newTopLeftDown;
 setInterval(function() {
     const leftRacket = document.querySelector('.left-racket img');
     const rightRacket = document.querySelector('.right-racket img');
@@ -83,7 +89,7 @@ setInterval(function() {
 
     if (moveUpRight) {
         // Move the right racket up
-        let newTopRightUp = (parseInt(rightRacket.style.top) || 0) - step;
+        newTopRightUp = (parseInt(rightRacket.style.top) || 0) - step;
         if (newTopRightUp >= -boardHeight / 2) {
             rightRacket.style.top = newTopRightUp + 'px';
         }
@@ -91,7 +97,7 @@ setInterval(function() {
 
     if (moveDownRight) {
         // Move the right racket down
-        let newTopRightDown = (parseInt(rightRacket.style.top) || 0) + step;
+        newTopRightDown = (parseInt(rightRacket.style.top) || 0) + step;
         if (newTopRightDown <= boardHeight / 2 - 100) {
             rightRacket.style.top = newTopRightDown + 'px';
         }
@@ -99,7 +105,7 @@ setInterval(function() {
 
     if (moveUpLeft) {
         // Move the left racket up
-        let newTopLeftUp = (parseInt(leftRacket.style.top) || 0) - step;
+        newTopLeftUp = (parseInt(leftRacket.style.top) || 0) - step;
         if (newTopLeftUp >= -boardHeight / 2) {
             leftRacket.style.top = newTopLeftUp + 'px';
         }
@@ -107,7 +113,7 @@ setInterval(function() {
 
     if (moveDownLeft) {
         // Move the left racket down
-        let newTopLeftDown = (parseInt(leftRacket.style.top) || 0) + step;
+        newTopLeftDown = (parseInt(leftRacket.style.top) || 0) + step;
         if (newTopLeftDown <= boardHeight / 2 - 100) {
             leftRacket.style.top = newTopLeftDown + 'px';
         }
@@ -165,17 +171,14 @@ setInterval(function() {
 // });
 
 //--------------------------ball------------------------------------\\
-
 const ballDiameter = ball.clientWidth;
-
+const leftRacketPos = leftRacket.offsetTop;
 let ballX = boardWidth / 2 - ballDiameter / 2 + rect.top; // Initial X position at the center of the board
 let ballY = boardHeight / 2 - ballDiameter / 2 + rect.left; // Initial Y position
 let speedX = 10; // Horizontal speed
 let speedY = 10; // Vertical speed
 
-let isMoving = false; // Flag to check if the ball is moving
-
-//listen for events to start the ball movement
+let isMoving = false;
 	document.addEventListener('keydown', function() {
 		isMoving = true;
 	});
@@ -190,45 +193,48 @@ let scoreP2_html = document.querySelector('.user-2-score > h2');
 var racket = document.getElementsByClassName('left-racket')[0];
 
 
-function moveBall() {
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const initleftRacketRect = leftRacket.getBoundingClientRect();
+const initrightRacketRect = rightRacket.getBoundingClientRect();
+let newChance;
+async function moveBall() {
 	if (!isMoving) {
 		requestAnimationFrame(moveBall);
         return;
     }
+	if (newChance) {
+			ball.style.left = `${ballX}px`;
+    		ball.style.top = `${ballY}px`;
+			await sleep(700);
+			moveBall;
+	}
+	newChance = false;
 	
 	scoreP1_html.innerHTML = scoreP1;
 	scoreP2_html.innerHTML = scoreP2;
     ballX += speedX;
     ballY += speedY;
-
     // Check for collision with the walls and reverse direction if needed
 	let ballRect = ball.getBoundingClientRect();
 	let leftRacketRect = leftRacket.getBoundingClientRect();
 	let rightRacketRect = rightRacket.getBoundingClientRect();
     if (ballX + ballDiameter + 10 > rect.right - ballDiameter) {
-		// scoreP2++;
-		// speedX = -speedX;
-		console.log("ballRect.left " + ballRect.left + " rightRacketRect.right " + rightRacketRect.right + " ballRect.top " + ballRect.top + " rightRacketRect.top " + rightRacketRect.top + " rightRacketRect.bottom " + rightRacketRect.bottom);
 		if (ballRect.top + ballRect.height >= rightRacketRect.top && ballRect.top <= rightRacketRect.bottom)
 		{
 			speedX = -speedX;
-			// ballX = rect.right;
 		}
 		else {
 			scoreP2++;
 			ballX = boardWidth / 2 - ballDiameter / 2 + rect.top;
 			ballY = boardHeight / 2 - ballDiameter / 2 + rect.left;
-			ball.style.left = `${ballX}px`;
-    		ball.style.top = `${ballY}px`;
-			return;
+			newChance = true;
 		}
 	}
 	if (ballX + 10 < rect.left) {
-		console.log("ballRect.left " + ballRect.left + " leftRacketRect.right " + leftRacketRect.right + " ballRect.top " + ballRect.top + " leftRacketRect.top " + leftRacketRect.top + " leftRacketRect.bottom " + leftRacketRect.bottom);
-		// console.log("board " + (rect.left + leftRacketRect.width + ballRect.width));
-
-		// if (ballRect.left <= (leftRacketRect.left+50))
-		// if (ballRect.left <= leftRacketRect.right && ballRect.top + ballRect.height >= leftRacketRect.top && ballRect.top <= leftRacketRect.bottom)
+		
 		if (ballRect.top + ballRect.height >= leftRacketRect.top && ballRect.top <= leftRacketRect.bottom)
 		{
 			speedX = -speedX;
@@ -238,9 +244,7 @@ function moveBall() {
 			scoreP1++;
 			ballX = boardWidth / 2 - ballDiameter / 2 + rect.top;
 			ballY = boardHeight / 2 - ballDiameter / 2 + rect.left;
-			ball.style.left = `${ballX}px`;
-    		ball.style.top = `${ballY}px`;
-			return;
+			newChance = true;
 		}
 	}
 	if (ballY + ballDiameter + 10 > rect.bottom) {
